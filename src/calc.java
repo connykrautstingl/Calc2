e02d69d6a43a5479d98fd0fe8e02ef0dc8b214ca
Connykrautstingl <s44784@edu.campus02.at>
2021-10-09 09:53:13 +0200

    add if statemant

---
+++ src/calc.java
@@ -0,0 +1,29 @@
+public class calc {
+
+
+    public static void main(String[] args) {
+
+        int a = 13;
+        int b = 4;
+
+    //    if divide(35,4){
+    //        return;
+    //        System.out.println("Ich bin größer als 10");
+    //    }
+
+    }
+
+    public static int divide(int dividend, int divisor) {
+        if (divisor == 0) {
+            return 0;
+        }
+        return dividend / divisor;
+    }
+
+
+    }
+
+
+
+
+
